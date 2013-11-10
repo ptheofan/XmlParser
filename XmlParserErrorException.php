e7966924ef3b5b2212a489f06ef3b66e94663508
Paris <ptheofan@gmail.com>
2013-11-10 17:04:38 +0100

    Update XmlParserErrorException.php

--- XmlParserErrorException.php
+++ XmlParserErrorException.php
@@ -11,11 +11,11 @@ class XmlParserErrorException extends Exception {
 
     public function __construct($parser, Exception $previous = null) {
         $this->errorInformation = array(
-            'errorCode' => xml_get_error_code(parser),
-            'errorString' => xml_error_string(xml_get_error_code(parser)),
-            'line' => xml_get_current_line_number(parser),
-            'column' => xml_get_current_column_number(parser),
-            'byteIdx' => xml_get_current_byte_index(parser),
+            'errorCode' => xml_get_error_code($parser),
+            'errorString' => xml_error_string(xml_get_error_code($parser)),
+            'line' => xml_get_current_line_number($parser),
+            'column' => xml_get_current_column_number($parser),
+            'byteIdx' => xml_get_current_byte_index($parser),
         );
 
         parent::__construct($this->errorInformation['errorString'], $this->errorInformation['errorCode'], $previous);
